7da497ffd08dbcd6c9a2eb932695d4f053d95382
Mohamed Zaki <37981931+MohamedZakii@users.noreply.github.com>
2021-01-12 19:39:04 +0200

    Update Parser.cs

--- TinyCompiler/Parser.cs
+++ TinyCompiler/Parser.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace TinyCompiler
 {
@@ -58,6 +62,317 @@ namespace TinyCompiler
             }
 
             return null;
+        }
+
+        Node Program()
+        {
+            //Program -> Functions mainFunction
+            Node prog = new Node("Program");
+
+            prog.children.Add(Functions());
+            prog.children.Add(mainFunction());
+
+            return prog;
+        }
+
+        private Node mainFunction()
+        {
+            //mainFunction -> Datatype Identifier
+            Node main = new Node("mainFunction");
+
+            main.children.Add(match(Datatype()));
+            main.children.Add(match(Token_Class.Identifier));
+
+            return main;
+        }
+        private Node Functions()
+        {
+            //Functions -> FunctionStatement FunctionStatementDash | FunctionStatementDash
+            Node Functions = new Node("Functions");
+
+            Functions.children.Add(FunctionStatement());
+            Functions.children.Add(FunctionStatementDash());
+
+            return Functions;
+
+        }
+
+        private Node FunctionStatementDash()
+        {
+            //FunctionStatmentDash -> FunctionStatment FunctionStatmentDash | E
+            Node FunDash = new Node("FunctionStatementDash");
+
+            FunDash.children.Add(FunctionStatement());
+            FunDash.children.Add(FunctionStatementDash());
+
+            return FunDash;
+        }
+
+        private Node FunctionStatement()
+        {
+            //FunctionStatment -> FunctionDecleration FunctionBody
+            Node FunState = new Node("FunctionStatment");
+
+            FunState.children.Add(FunctionDecleration());
+            FunState.children.Add(FunctionBody());
+
+            return FunState;
+
+        }
+
+        private Node FunctionDecleration()
+        {
+            //FunctionDecleration -> FunctionName ( Parameter )
+            Node FunDec = new Node("FunctionDecleration");
+
+            FunDec.children.Add(FunctionName());
+            FunDec.children.Add(Parameter());
+
+            return FunDec;
+        }
+
+        private Node FunctionBody()
+        {
+            // FunctionBody -> { Statments RetunStatment }
+            Node FunBody = new Node("FunctionBody");
+            if (check(Token_Class.LBraces))
+            {
+                FunBody.children.Add(match(Token_Class.LBraces));
+                FunBody.children.Add(Statements());
+                FunBody.children.Add(ReturnStatements());
+                FunBody.children.Add(match(Token_Class.RBraces));
+
+                return FunBody;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Node ReturnStatements()
+        {
+            //ReturnStatement -> return Expression ; 
+            Node Rs = new Node("ReturnStatement");
+            if (check(Token_Class.Return))
+            {
+                Rs.children.Add(match(Token_Class.Return));
+                Rs.children.Add(Expression());
+
+                return Rs;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private Node Statements()
+        {
+            //Statements -> Statement MoreStatements
+            Node S = new Node("Statements");
+
+            S.children.Add(Statement());
+            S.children.Add(MoreStatments());
+
+            return S;
+        }
+
+        private Node MoreStatments()
+        {
+            //MoreStatements -> ; Statement MoreStatements | E
+            Node MS = new Node("MoreStatements");
+
+            if (check(Token_Class.Semicolon))
+            {
+                MS.children.Add(match(Token_Class.Semicolon));
+                MS.children.Add(Statement());
+                MS.children.Add(MoreStatments());
+
+                return MS;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Node Statement()
+        {
+            //Statement -> Comment | Assignment | Decleration | WriteStatement | ReadStatement | ReturnStatement | if_Statement | RepeatStatement | E
+            Node Statment = new Node("Statement");
+
+            //if(check(Token_Class.Return))
+            Statment.children.Add(ReturnStatements());
+            Statment.children.Add(if_Statement());
+            Statment.children.Add(RepeatStatement());
+
+            return Statment;
+            
+
+        }
+
+        private Node RepeatStatement()
+        {
+            // RepeatStatements -> repeat Statements Until ConditionStatement
+            Node Repeat = new Node("RepeatStatment");
+
+            if (check(Token_Class.Repeat))
+            {
+                Repeat.children.Add(match(Token_Class.Repeat));
+                Repeat.children.Add(Statements());
+                Repeat.children.Add(match(Token_Class.Until));
+                Repeat.children.Add(ConditionStatement());
+
+                return Repeat;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Node ConditionStatement()
+        {
+            // ConditionStatement -> Condition MoreConditions
+            Node Cs = new Node("ConditionStatement");
+
+            Cs.children.Add(Condition());
+            Cs.children.Add(MoreConditions());
+
+            return Cs;
+        }
+
+        private Node MoreConditions()
+        {
+            //MoreCondition -> BoolOP Condition MoreConditions | E
+            Node MC = new Node("MoreConditions");
+
+            MC.children.Add(BoolOP());
+            MC.children.Add(Condition());
+            MC.children.Add(MoreConditions());
+
+            return MC;
+        }
+
+        private Node BoolOP()
+        {
+            //BoolOP -> "||" | &&
+            Node Bol = new Node("BoolOP");
+
+            if (check(Token_Class.AndOp))
+            {
+                Bol.children.Add(match(Token_Class.AndOp));
+            }
+            else if (check(Token_Class.OrOp))
+            {
+                Bol.children.Add(match(Token_Class.OrOp));
+            }
+            else
+            {
+                return null;
+            }
+            return Bol;
+        }
+
+        private Node Condition()
+        {
+            //Condition -> Identifier CondiotionOP Term
+            Node Cond = new Node("Condition");
+
+            if (check(Token_Class.Identifier))
+            {
+                Cond.children.Add(match(Token_Class.Identifier));
+                Cond.children.Add(CondiotionOP());
+                Cond.children.Add(Term());
+
+                return Cond;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        private Node CondiotionOP()
+        {
+            //ConditonOP -> < | > | = | <>
+            Node Co = new Node("ConditionOP");
+
+            if (check(Token_Class.LessThanOp))
+            {
+                Co.children.Add(match(Token_Class.LessThanOp));
+            }
+            else if (check(Token_Class.GreaterThanOp))
+            {
+                Co.children.Add(match(Token_Class.GreaterThanOp));
+            }
+            else if (check(Token_Class.BooleanEqual))
+            {
+                Co.children.Add(match(Token_Class.BooleanEqual));
+            }
+            else if (check(Token_Class.NotEqualOp))
+            {
+                Co.children.Add(match(Token_Class.NotEqualOp));
+            }
+
+            return Co;
+        }
+
+        private Node if_Statement()
+        {
+            //if_Statement -> if CondationStatement then Statements ElseClause
+            Node ifStat = new Node("if_Statement");
+
+            if (check(Token_Class.If))
+            {
+                ifStat.children.Add(match(Token_Class.If));
+                ifStat.children.Add(ConditionStatement());
+                ifStat.children.Add(match(Token_Class.Then));
+                ifStat.children.Add(Statements());
+                ifStat.children.Add(ElseClause());
+
+                return ifStat;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Node ElseClause()
+        {
+            //ElseClause -> ElseStatements End | End
+            Node ElseC = new Node("ElseClause");
+
+            ElseC.children.Add(ElseStatements());
+            ElseC.children.Add(match(Token_Class.End));
+
+            return ElseC;
+        }
+
+        private Node ElseStatements()
+        {
+            //ElseStatements -> elseif_Statement | else_Statement
+            Node ES = new Node("ElseStatements");
+
+            ES.children.Add(ElseIf());
+            ES.children.Add(Else());
+
+            return ES;
+        }
+
+        private Node Else()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Node ElseIf()
+        {
+            Node ei = new Node("ElseIF");
+
+            if(check(Token_Class.ElseIf))
+            ei.children.Add()
         }
 
         Node FunctionCall()
